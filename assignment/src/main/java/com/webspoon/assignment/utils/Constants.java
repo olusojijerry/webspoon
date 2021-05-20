48562df93cc555f9d43602b0b2ccc45e7939e41a
unknown <olusojijerry@outlook.com>
2021-05-21 00:59:21 +0100

    removed unwanted files

--- assignment/src/main/java/com/webspoon/assignment/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.webspoon.assignment.utils;
-
-public class Constants {
-    public static final String BASE_URL = "localhost:9092/snippets/recipe";
-    public static final Integer SECONDS_REQUIRED = 30;
-}
--- assignment/src/test/java/com/webspoon/assignment/endpoint/SnippetApiTest.java
+++
@@ -1,7 +0,0 @@
-package com.webspoon.assignment.endpoint;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class SnippetApiTest {
-
-}
